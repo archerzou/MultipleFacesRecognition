6aaa46872f7920ce92796b4579395a8bab629d95
Viquaruddin Ahmed <viquar10@gmail.com>
2018-07-29 05:15:19 +0200

    Update server.js

--- MultipleFacesBack-end/server.js
+++ MultipleFacesBack-end/server.js
@@ -14,8 +14,8 @@ const db = knex({
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : '24091993Vi$',
-    database : 'smart-brain'
+    password : '',
+    database : 'multiple-faces'
   }
 });
 
