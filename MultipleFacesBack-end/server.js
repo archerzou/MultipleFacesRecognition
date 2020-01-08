24a91fe96d8452c01849298c5aae0d27c9d901e7
kevinwit <kevin.zou@honeywell.com>
2020-01-08 10:48:58 +0800

    update the server.js

--- MultipleFacesBack-end/server.js
+++ MultipleFacesBack-end/server.js
@@ -19,12 +19,21 @@ const db = knex({
   }
 });
 
+// deploy on heroku 
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true  
+//   }
+// });
+
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> { res.send('It is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
@@ -34,3 +43,8 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
 })
+
+// deploy on Heroku
+// app.listen(process.env.PORT || 3000, ()=> {
+//   console.log('app is running on port ${process.env.PORT}');
+// })
